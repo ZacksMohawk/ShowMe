b31b246d6f62731ed4831ae08cb735eae25ac3c8
Matty Mahoney <matty@zacksmohawk.com>
2024-06-07 13:12:02 +0100

    Added 'editshowme' alias to setup scripts.

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
 
 touch ~/.bashrc
+
+SHOWMESET=false
+EDITSHOWMESET=false
+
 while read -r line
 do
 	if [[ "$line" =~ ^"alias showme="* ]]; then
-		echo "Alias already set"
-		exit 1
+		SHOWMESET=true
 	fi
 done < ~/.bashrc
 
-echo "Setting 'showme' alias";
+while read -r line
+do
+	if [[ "$line" =~ ^"alias editshowme="* ]]; then
+		EDITSHOWMESET=true
+	fi
+done < ~/.bashrc
+
+NEWLINESET=false
+
+if [[ "$SHOWMESET" != true ]]; then
+	if [[ "$NEWLINESET" != true ]]; then
+		echo '' >> ~/.bashrc
+		NEWLINESET=true
+	fi
+	echo "Setting 'showme' alias";
 echo "alias showme='dt=\$(pwd); cd $(pwd); node ShowMe.js -folderPath \$dt; cd \$dt;'" >> ~/.bashrc
+fi
+
+if [[ "$EDITSHOWMESET" != true ]]; then
+	if [[ "$NEWLINESET" != true ]]; then
+		echo '' >> ~/.bashrc
+		NEWLINESET=true
+	fi
+	echo "Setting 'editshowme' alias";
+echo "alias editshowme='touch ~/show.me; open ~/show.me'" >> ~/.bashrc
+fi
+
 source ~/.bashrc
--- setup_mac.sh
+++ setup_mac.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
 
 touch ~/.zshrc
+
+SHOWMESET=false
+EDITSHOWMESET=false
+
 while read -r line
 do
 	if [[ "$line" =~ ^"alias showme="* ]]; then
-		echo "Alias already set"
-		exit 1
+		SHOWMESET=true
 	fi
 done < ~/.zshrc
 
-echo "Setting 'showme' alias";
+while read -r line
+do
+	if [[ "$line" =~ ^"alias editshowme="* ]]; then
+		EDITSHOWMESET=true
+	fi
+done < ~/.zshrc
+
+NEWLINESET=false
+
+if [[ "$SHOWMESET" != true ]]; then
+	if [[ "$NEWLINESET" != true ]]; then
+		echo '' >> ~/.zshrc
+		NEWLINESET=true
+	fi
+	echo "Setting 'showme' alias";
 echo "alias showme='dt=\$(pwd); cd $(pwd); node ShowMe.js -folderPath \$dt; cd \$dt;'" >> ~/.zshrc
+fi
+
+if [[ "$EDITSHOWMESET" != true ]]; then
+	if [[ "$NEWLINESET" != true ]]; then
+		echo '' >> ~/.zshrc
+		NEWLINESET=true
+	fi
+	echo "Setting 'editshowme' alias";
+echo "alias editshowme='touch ~/show.me; open ~/show.me'" >> ~/.zshrc
+fi
+
 source ~/.zshrc
